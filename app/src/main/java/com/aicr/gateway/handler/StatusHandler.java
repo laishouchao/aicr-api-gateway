dad052fb2ea850dcad8f2d40987f9dcfdccedeb9
来SC <55373024+laishouchao@users.noreply.github.com>
2026-08-28 17:31:42 +0800

    Add debug endpoint to list AIDL proxy methods for diagnosis

--- app/src/main/java/com/aicr/gateway/handler/StatusHandler.java
+++ app/src/main/java/com/aicr/gateway/handler/StatusHandler.java
@@ -1,7 +1,6 @@
 package com.aicr.gateway.handler;
 
 import com.aicr.gateway.hook.ServiceProxy;
-import com.aicr.gateway.util.LogUtil;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import fi.iki.elonen.NanoHTTPD;
@@ -9,6 +8,7 @@ import fi.iki.elonen.NanoHTTPD.IHTTPSession;
 import fi.iki.elonen.NanoHTTPD.Method;
 import fi.iki.elonen.NanoHTTPD.Response;
 import fi.iki.elonen.NanoHTTPD.Response.Status;
+import java.lang.reflect.Method;
 
 public class StatusHandler extends BaseHandler {
 
@@ -33,8 +33,45 @@ public class StatusHandler extends BaseHandler {
             JsonArray capabilities = new JsonArray();
             capabilities.add("ocr"); capabilities.add("ner"); capabilities.add("segment");
             data.add("capabilities", capabilities);
+
+            // Debug mode: list AIDL proxy methods
+            String query = session.getQueryParameterString();
+            if (query != null && query.contains("debug")) {
+                JsonObject debug = new JsonObject();
+                listMethods(debug, "visionProxy", serviceProxy.getVisionService());
+                listMethods(debug, "nerProxy", serviceProxy.getNerService());
+                listMethods(debug, "segmentProxy", serviceProxy.getSegmentService());
+                data.add("debug", debug);
+            }
+
             responseJson.add("data", data);
             return NanoHTTPD.newFixedLengthResponse(Status.OK, "application/json", responseJson.toString());
         } catch (Exception e) { return buildErrorResponse(500, "Error: " + e.getMessage()); }
+    }
+
+    private void listMethods(JsonObject parent, String key, Object service) {
+        JsonArray methods = new JsonArray();
+        if (service != null) {
+            Class<?> clazz = service.getClass();
+            while (clazz != null && clazz != Object.class) {
+                for (Method m : clazz.getDeclaredMethods()) {
+                    JsonObject mObj = new JsonObject();
+                    mObj.addProperty("name", m.getName());
+                    mObj.addProperty("class", m.getDeclaringClass().getSimpleName());
+                    Class<?>[] params = m.getParameterTypes();
+                    JsonArray pArr = new JsonArray();
+                    for (Class<?> p : params) pArr.add(p.getSimpleName());
+                    mObj.add("params", pArr);
+                    mObj.addProperty("return", m.getReturnType().getSimpleName());
+                    methods.add(mObj);
+                }
+                clazz = clazz.getSuperclass();
+            }
+        } else {
+            JsonObject nullObj = new JsonObject();
+            nullObj.addProperty("error", "service proxy is null");
+            methods.add(nullObj);
+        }
+        parent.add(key, methods);
     }
 }
